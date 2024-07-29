70643c8124020a94edcf8f4db40fa770a99579bc
jeongjihun <wlgns2234@naver.com>
2024-07-29 12:42:32 +0900

    feat: 게시판 통합 테스트 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,9 +36,16 @@ subprojects {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
 
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+        // kotest
         testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
         testImplementation("io.kotest:kotest-assertions-core:5.9.1")
         testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
+
+        // test container
+        testImplementation("org.testcontainers:testcontainers")
+        testImplementation("org.testcontainers:mysql")
+
     }
 
     kotlin {
