e8db12abf9e4e5816ea049e8c09b204257356544
jeongjihun <wlgns2234@naver.com>
2025-11-19 00:59:07 +0900

    ttt

--- ontongsal-websocket/src/main/kotlin/yjh/ontongsal/websocket/stomp/StompController.kt
+++ ontongsal-websocket/src/main/kotlin/yjh/ontongsal/websocket/stomp/StompController.kt
@@ -3,6 +3,7 @@ package yjh.ontongsal.websocket.stomp
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.messaging.simp.SimpMessageSendingOperations
 import org.springframework.stereotype.Controller
 import org.springframework.web.util.HtmlUtils
 
@@ -12,15 +13,32 @@ import org.springframework.web.util.HtmlUtils
  *      실제로는 클라이언트가 publish/chatting/999 로 메세지를 보내면, @MessageMapping 으로 라우팅됩니다.
  */
 @Controller
-class StompController {
+class StompController(
+    private val simpMessageSendingOperations: SimpMessageSendingOperations,
+) {
 
+    // 방법1
     @MessageMapping("/chatting/{roomId}") // 클라이언트는 "/publish/chatting/123" 라는 경로로 메세지를 보낸다.
     @SendTo("/topic/{roomId}") // return 값에 담긴 내용은 "/topic/123"를 구독하고 있는 모든 사람들에게 전파된다.
-    fun chatting(
+    fun chattingVersion1(
         @DestinationVariable roomId: Long,
-        chattingMessage: ChattingMessage
+        chattingMessage: ChattingMessage,
     ): ChattingResponse {
         println("controller : 메세지를 받았습니다 >>> ${chattingMessage.message}")
         return ChattingResponse(HtmlUtils.htmlEscape(chattingMessage.message))
+    }
+
+    // 방법2
+    @MessageMapping("/chatting/{roomId}")
+    fun chattingVersion2(
+        @DestinationVariable roomId: Long,
+        chattingMessage: ChattingMessage,
+    ) {
+        println("controller : 메세지를 받았습니다 >>> ${chattingMessage.message}")
+
+        simpMessageSendingOperations.convertAndSend(
+            "/topic/$roomId",
+            ChattingResponse(HtmlUtils.htmlEscape(chattingMessage.message))
+        )
     }
 }
