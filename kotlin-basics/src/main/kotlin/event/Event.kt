f905e8cdb310a35996fceec6a906d12bd25a9bd6
jeongjihun <wlgns2234@naver.com>
2026-06-18 22:20:17 +0900

    step2

--- kotlin-basics/src/main/kotlin/event/Event.kt
+++ kotlin-basics/src/main/kotlin/event/Event.kt
@@ -1,11 +1,16 @@
 package event
 
+/**
+ * Event <-> Policy 를 연결한다(타입 안전하게)
+ *
+ * Mission 이벤트는 MissionEventPolicy 만 가진다.
+ * 컴포지션도 좋지만, 직접 타입 안전하게 제어하기 위해 제너릭 out 을 사용한다.
+ */
+sealed interface Event<out P : EventPolicy> {
 
-sealed interface Event {
+    data object Mission : Event<EventPolicy.MissionEventPolicy>
 
-    data object Mission : Event
+    data object Attendance : Event<EventPolicy.AttendanceEventPolicy>
 
-    data object Attendance : Event
-
-    data object Invite : Event
+    data object Invite : Event<EventPolicy.InvitationEventPolicy>
 }
